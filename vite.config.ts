c7d777d747c186bef790c827b7013e8e213f2343
Henry Stelle <high.over.low@gmail.com>
2026-02-26 19:50:37 -0500

    Fix GH and local dev server issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/watching-ripples/" : "/",
+  plugins: [tailwindcss(), react(), wasm(), topLevelAwait()],
   worker: {
     format: "es",
-    plugins: () => [wasm()],
+    plugins: () => [wasm(), topLevelAwait()],
   },
-});
+}));
