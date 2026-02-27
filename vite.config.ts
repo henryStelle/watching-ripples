ad837c88c338bb7d2b98bbf6a939ddfa8a60f1f8
Henry Stelle <hstelle@uwaterloo.ca>
2026-02-26 19:30:56 -0500

    Try and publish to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), wasm(), topLevelAwait()],
+  plugins: [tailwindcss(), react()],
+  worker: {
+    format: "es",
+    plugins: () => [wasm()],
+  },
 });
